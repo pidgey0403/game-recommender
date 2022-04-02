df7a794ad23bbb6187e23c6f33a32c01ee67cc24
Gabby Niamat <gniamat@uwo.ca>
2022-04-02 01:24:52 -0400

    working version pre cucai

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+//bug: takes 2 presses to display results initially
 
-function submitForm(){
+function submitForm() {
     nameValue = document.getElementById("searchTerm").value;
-    //bug: takes 2 presses to display results initially
 
-    document.getElementById("display-results").innerHTML = nameValue;
+    //capitalize each letter of a new word
+    nameValue = nameValue.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+
+    document.getElementById("display-results").innerHTML = "You wanted games like: " + "<u>" + nameValue + "</u>" + "<br><br>" + "<h4> Check out the following titles: </h4>";
+
     location.href = "#page-3";
 
     function arrayToTable(tableData) {
         var table = $('<table></table>');
         $(tableData).each(function (i, rowData) {
             var row = $('<tr></tr>');
-            
             $(rowData).each(function (j, cellData) {
-              if (cellData == nameValue) {
-                row.append($('<td>'+rowData[1]+'</td>'));
-              }
+                if (cellData == nameValue) {
+                    row.append($('<td>' + rowData[1] + '</td>'));
+                }
             });
             table.append(row);
         });
@@ -25,8 +28,15 @@ function submitForm(){
         type: "GET",
         url: "mainFile.csv",
         success: function (data) {
-            parsed = Papa.parse(data).data;
-            $('#display-results').append(arrayToTable(Papa.parse(data).data));
+            var table = arrayToTable(Papa.parse(data).data); // convert parse object to table
+            table = table.eq(0).text(); // get text value of table 
+            table = table.replaceAll(/[\[\]"']+/g, ''); // replace brackets and quotations 
+            table = "<ul><li>" + table.replaceAll(",", '<li>')+"</ul>"; // create an unordered list display
+
+            if ($('#game-list').length === 0) // append if you haven't built the table yet
+                $('#game-list').append(table);
+            else
+                $('#game-list').replaceWith("<h4>" + table + "<\h4>");
         }
     });
 }
